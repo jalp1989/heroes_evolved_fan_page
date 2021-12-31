d78ef041870c481709a7a60833989bb8c0005822
jalp1989 <joseph.lizarme.prado@gmail.com>
2021-12-31 13:38:50 +0100

    se cambia puertos y package.json

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ const path = require('path');
 
 
 // settings
-app.set('port',3000);
-//app.set('port', process.env.PORT || 3000);
+//app.set('port',3000);
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine','ejs');
