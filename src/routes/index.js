de605012f8498e4fc75c2834ce136d29f8b534f9
jalp1989 <joseph.lizarme.prado@gmail.com>
2022-04-12 16:19:56 +0200

    Se eligen las fotos de portada y quienes somos

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,16 @@
  const router = express.Router();
 
 router.get('/', (req,res)=>{
-	res.render('index.html', { title: 'First Website'});	
+	res.render('index.html', { title: 'Inicio - CAIME'});	
 })
 
 router.get('/contact', (req,res)=>{
 	res.render('contact.html', { title: 'Contact Page'});	
 })
+
+router.get('/proyectos', (req,res)=>{
+	res.render('proyectos.html', { title: 'Proyectos - CAIME'});	
+})
+
+
 module.exports = router;
